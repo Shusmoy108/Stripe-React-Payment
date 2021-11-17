959142b6cc433d958e166ab7f4c78eafa8a6cc19
Shusmoy <1305108.sc@ugrad.cse.buet.ac.bd>
2021-11-17 11:22:42 +0600

    stripe

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import StripeCheckout from "react-stripe-checkout";
+import { Button } from "@material-ui/core";
+import axios from "axios";
+const API_URL = "http://localhost:8080";
 function App() {
+  const [product, setProduct] = useState({
+    name: "bill",
+    price: 10,
+    productby: "frontend",
+  });
+  const amount = 1000;
+  const makePayment = (token: any) => {
+    //const body={token,product};
+
+    const body = {
+      token,
+      clientId: "6178b45c681f02c87692f5e1",
+      clientName: "Girmairi Motors",
+      ownerName: "Shadman Girmairi",
+      description: "Payment with Stripe",
+
+      amount: amount,
+      refId: "Rx10112510",
+    };
+    //const headers = { "Content-Type": "application/json" };
+    axios.post(API_URL + "/api/payment/add", body).then((response) => {
+      console.log(response.data);
+    });
+
+    // fetch("http://localhost:8080/api/payment/pay", {
+    //   method: "POST",
+    //   headers,
+    //   body: JSON.stringify(body),
+    // })
+    //   .then((response: any) => {
+    //     console.log(response);
+    //     console.log(response.body["success"]);
+    //     alert(response.body["message"]);
+    //   })
+    //   .catch();
+    console.log(token);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +50,28 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <StripeCheckout
+          stripeKey=""
+          token={makePayment}
+          image="https://foyob-test-s3-1.s3.amazonaws.com/foyob/FOYOB_logo.png"
+          name="FOYOB Payment"
+          amount={amount * 100}
+          description="Service Fee"
+          panelLabel="Pay Bill"
+          allowRememberMe={true}
         >
-          Learn React
-        </a>
+          <Button
+            style={{
+              backgroundColor: "#2F77AD",
+              color: "white",
+              textTransform: "capitalize",
+            }}
+            variant="contained"
+          >
+            Complete Payment
+          </Button>
+        </StripeCheckout>
       </header>
     </div>
   );
